e47e39c2c32f2cca0b5e8e3241d63a2ec0812fac
Fred Wenzel <fwenzel@mozilla.com>
2012-10-15 15:05:05 -0700

    settings tweaks

--- settings.py
+++ settings.py
@@ -3,8 +3,15 @@ import socket
 
 # Commands to start and stop mjpg streaming server.
 START_SERVER = '/usr/bin/mjpg_streamer -i "input_uvc.so --yuv -d /dev/video0  -r 640x480 -f 2" -o "output_http.so -p 8090 -w /dev/null"'
-STOP_SERVER = 'killall mjpg_streamer'
-CHECK_SERVER = 'ps x | grep mjpg_streamer | grep -v grep'  # Exit code 0 if on, >0 otherwise.
+STOP_SERVER = '/usr/bin/killall mjpg_streamer'
+CHECK_SERVER = '/bin/ps x | /usr/bin/grep mjpg_streamer | /usr/bin/grep -v grep'  # Exit code 0 if on, >0 otherwise.
 
-# URL of MJPG stream
+# URL of MJPG stream (the image we'll show!)
 CAM_SRC = 'http://%s:8090/?action=stream' % socket.gethostname()
+
+
+# Import local settings file.
+try:
+	from local_settings import *
+except ImportError:
+	pass
